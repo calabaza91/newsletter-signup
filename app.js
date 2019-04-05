8fd9f2d2fc9ca045d8b709c05e6b039c9cd26c51
Caleb Cox <calebcox2010@gmail.com>
2019-04-04 23:55:46 -0500

    Update files to use css and imgs on server

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const request = require("request");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
+
+//Incorporates css, imgs files etc.
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
 app.listen(3000, function(){
   console.log('Server running on port 3000');
 });
