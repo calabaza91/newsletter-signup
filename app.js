ab8f8bc961d2d895b6795eff79127f593ebe34b0
Caleb Cox <calebcox2010@gmail.com>
2019-04-05 00:06:58 -0500

    Update app.js with post method

--- app.js
+++ app.js
@@ -5,13 +5,24 @@ const request = require("request");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
-
 //Incorporates css, imgs files etc.
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({extended:true}));
+
+
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+
+  var first = req.body.first;
+  var last = req.body.last;
+  var email = req.body.email;
+
+  console.log(`${first} ${last}: ${email}`);
+  
 });
 
 app.listen(3000, function(){
